583238182f61763082bf06f1d2c0c8f83c31895c
adnanzukic <adnan16zukic@gmail.com>
2026-03-24 19:28:12 +0100

    Dodan ObjectId i kategorije u Transaction model

--- src/financeapp/Transaction.java
+++ src/financeapp/Transaction.java
@@ -1,25 +1,40 @@
 package financeapp;
 
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class Transaction {
+    private ObjectId id;
     private String type;
     private double amount;
     private String description;
+    private String category;
 
-    public Transaction(String type, double amount, String description) {
+    public Transaction(String type, double amount, String description, String category) {
         this.type = type;
         this.amount = amount;
         this.description = description;
+        this.category = category;
+    }
+
+    public Transaction(ObjectId id, String type, double amount, String description, String category) {
+        this.id = id;
+        this.type = type;
+        this.amount = amount;
+        this.description = description;
+        this.category = category;
     }
 
     public Document toDocument() {
         return new Document("type", type)
                 .append("amount", amount)
-                .append("description", description);
+                .append("description", description)
+                .append("category", category);
     }
 
+    public ObjectId getId() { return id; }
     public String getType() { return type; }
     public double getAmount() { return amount; }
     public String getDescription() { return description; }
+    public String getCategory() { return category; }
 }
