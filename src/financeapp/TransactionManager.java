c8c69cd9d796f219b49e36afdc80435b5da9676d
adnanzukic <adnan16zukic@gmail.com>
2026-03-24 19:28:56 +0100

    Dodane funkcije za azuriranje i brisanje transakcija

--- src/financeapp/TransactionManager.java
+++ src/financeapp/TransactionManager.java
@@ -3,7 +3,10 @@ package financeapp;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 import java.util.ArrayList;
 
 public class TransactionManager {
@@ -18,15 +21,33 @@ public class TransactionManager {
         collection.insertOne(t.toDocument());
     }
 
+    public void updateTransaction(ObjectId id, String type, double amount, String description, String category) {
+        collection.updateOne(
+                Filters.eq("_id", id),
+                Updates.combine(
+                        Updates.set("type", type),
+                        Updates.set("amount", amount),
+                        Updates.set("description", description),
+                        Updates.set("category", category)
+                )
+        );
+    }
+
+    public void deleteTransaction(ObjectId id) {
+        collection.deleteOne(Filters.eq("_id", id));
+    }
+
     public ArrayList<Transaction> getAllTransactions() {
         ArrayList<Transaction> list = new ArrayList<>();
         MongoCursor<Document> cursor = collection.find().iterator();
         while (cursor.hasNext()) {
             Document d = cursor.next();
             list.add(new Transaction(
+                    d.getObjectId("_id"),
                     d.getString("type"),
                     d.getDouble("amount"),
-                    d.getString("description")
+                    d.getString("description"),
+                    d.getString("category") != null ? d.getString("category") : "Ostalo"
             ));
         }
         return list;
@@ -46,6 +67,16 @@ public class TransactionManager {
         double total = 0;
         for (Transaction t : getAllTransactions()) {
             if (t.getType().equals("Rashod")) {
+                total += t.getAmount();
+            }
+        }
+        return total;
+    }
+
+    public double getExpenseByCategory(String category) {
+        double total = 0;
+        for (Transaction t : getAllTransactions()) {
+            if (t.getType().equals("Rashod") && t.getCategory().equals(category)) {
                 total += t.getAmount();
             }
         }
