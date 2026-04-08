96682cffeda290556db7a30a739e9ea73df872d9
adnanzukic <adnan16zukic@gmail.com>
2026-04-08 23:39:11 +0200

    Azuriran Main.java da otvara LoginForm

--- src/financeapp/Main.java
+++ src/financeapp/Main.java
@@ -5,10 +5,10 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Praćenje ličnih finansija");
-            frame.setContentPane(new FinanceTrackerForm().getMainPanel());
+            JFrame frame = new JFrame("Life Management System");
+            frame.setContentPane(new LoginForm().getMainPanel());
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 550);
+            frame.setSize(500, 450);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
